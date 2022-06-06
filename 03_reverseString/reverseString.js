8ce67d9313c561711ea139419897f985496f96c1
betofth <omluizalberto@gmail.com>
2022-06-06 16:49:56 -0300

    Finished 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let lstr = str.length;
+    let ans = "";
+    for (let i = lstr-1; i>=0; i--){
+        ans = ans + str[i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
