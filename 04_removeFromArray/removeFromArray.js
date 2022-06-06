a66fe242e121d4142c7ad59df05bda95661cbbb0
betofth <omluizalberto@gmail.com>
2022-06-06 18:19:43 -0300

    Finished 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, arg1, ...args) {
+    let arrayLength = array.length;
+    for (let i = 0; arrayLength>i; i++){
+        let argsLength = args.push(arg1) //joga arg1 como ultimo item de args e retorna o tamanho
+        for (let a = 0; args.length>a; a++){
+            if (array[i] == args[a]){
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
