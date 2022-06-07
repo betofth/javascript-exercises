afad3486d27d7987ed003b12c13f025cb9c2f31f
betofth <omluizalberto@gmail.com>
2022-06-07 00:43:09 -0300

    Finished 6th exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
-};
+const leapYears = function(ano) {
+    if(ano%4 == 0){
+        if (ano%100 == 0){
+            if (ano%400 == 0){
+                return true;
+            }
+            else return false;
+        }
+        return true;
+    } else return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
