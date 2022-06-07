64db21855f641a1e7f3a3c4b89257cf97ee23ec5
betofth <omluizalberto@gmail.com>
2022-06-07 00:14:49 -0300

    Finished 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    let x = 0;
+    if(typeof n1 != "number" || typeof n2 != "number"){
+        return "ERROR";
+    }
+    if (n1<0 || n2<0){
+        return "ERROR";
+    }
+    if(n2>n1){
+        for (let i = n1; n2>=i; i++){
+        x = x + i;
+        }   
+        return x;
+    }
+    else if(n1>n2){
+        for(let i = n2; n1>=i; i++){
+            x = x + i;
+        }
+        return x;
+    }
 };
 
 // Do not edit below this line
