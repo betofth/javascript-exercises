dccc05346207091759b2bd7de68120dec2f47c22
betofth <omluizalberto@gmail.com>
2022-06-07 00:56:15 -0300

    Finished 6th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fnum) {
+    let x = Number(fnum);
+    let y = (x-32)*(5/9);
+    y = Number(y.toFixed(1));
+    return y;
 };
 
-const ctof = function() {
-
+const ctof = function(cnum) {
+    let x = Number(cnum);
+    let y = cnum*(9/5) + 32;
+    y = Number(y.toFixed(1));
+    return y;
 };
 
 // Do not edit below this line
