d6748e7a6dec3bb77187ba102ca75b8c8269d3b3
betofth <omluizalberto@gmail.com>
2022-06-08 16:44:35 -0300

    Finished 11th exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arrLength = books.length;
+    let res = [];
+    let aux;
+    for (let i = 0; arrLength > i; i++){
+        aux = books[i];
+        res[i] = aux["title"];
+    }
+    return res;
 };
 
 // Do not edit below this line
