78c7f48c7dc52474dc0f8b312a455cda8368e372
betofth <omluizalberto@gmail.com>
2022-06-08 17:04:44 -0300

    Finished 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let aux;
+    let peopleLength = people.length;
+    let maxi;
+    let age;
+    let maxAge;
+    let ans = [];
+    for (i = 0; peopleLength>i; i++){
+        aux = people[i];
+        age = aux["yearOfDeath"] - aux["yearOfBirth"];
+        if (aux.hasOwnProperty('yearOfDeath') == false){
+            age = 2022 - aux["yearOfBirth"];
+        }
+        if (i == 0){
+            maxAge = age;
+            maxi = i;
+        } else {
+            if (age > maxAge){
+                maxAge = age;
+                maxi = i;
+            }
+        }
+    }
+    ans = people[maxi];
+    return ans;
 };
 
 // Do not edit below this line
