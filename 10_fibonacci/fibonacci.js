65fbf83f54ecc0b217d52a24f25a0bbcb3e40916
betofth <omluizalberto@gmail.com>
2022-06-08 16:32:58 -0300

    Finished 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(seqSize) {
+    seqSize = Number(seqSize);
+    let aux0 = 0;
+    let aux1 = 1;
+    let fiboN = 1;
+    if (0>seqSize){
+        return "OOPS";
+    }
+    for (let i = 1; seqSize>i; i++){
+        fiboN = aux0 + aux1;
+        aux0 = aux1;
+        aux1 = fiboN;
+    }
+    return fiboN;
 };
 
 // Do not edit below this line
