adc2e47fef767e81a1a51070d329410cf2705693
betofth <omluizalberto@gmail.com>
2022-06-08 16:23:44 -0300

    Finished 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let wordReverse = "";
+    word = word.replace(/[^\w\s\']|_/g, "");
+    word = word.toLowerCase();
+    word = word.replace(/\s+/g, '');
+    let wordLength = word.length;
+    let wordArray = Array.from(word);
+    for (let i = wordLength-1; i>=0; i--){
+        wordReverse += wordArray[i];
+    }
+    if (wordReverse == word){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
